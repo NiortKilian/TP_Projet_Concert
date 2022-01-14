9b6709a776731acee90b3f2111d7277ff13048f8
Niort Kilian <kilian.niort@etud.fr>
2022-01-14 14:51:54 +0100

    test env

--- src/Controller/BandController.php
+++ src/Controller/BandController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Artist;
 use App\Entity\Band;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -26,7 +27,20 @@ class BandController extends AbstractController
         $band = $bandRepo->findAll();
         return $this->render('band/list.html.twig', [
             'controller_name' => 'BandController',
-            'groupes' => $band,
+            'bands' => $band,
+        ]);
+    }
+
+    /**
+     * @Route(/bands/{id} name="band_show")
+     */
+    public function showAction(): Response
+    {
+        $artistRepo = $this->getDoctrine()->getRepository(Artist::class);
+        $artist = $artistRepo->findAll();
+        return $this->render('band/show.html.twig',[
+           'controller-name' => 'BandController',
+           'artists' => $artist,
         ]);
     }
 }
